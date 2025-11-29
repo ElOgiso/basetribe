daed7c1eabd69a77f6c77da4cf9609eb26ba3ffd
ElOgiso <otaboremriss@gmail.com>
2025-11-29 13:55:08 -0800

    Fix: Add Wagmi providers and custom Manifold mint button

--- api/manifold.ts
+++ api/manifold.ts
@@ -10,10 +10,7 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
-  
-  // Always return JSON
-  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
